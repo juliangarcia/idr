8721bdbf24e3570d01df1b058ea4490dc4045c98
MathewBaker <bakermg@outlook.com>
2020-07-21 12:19:25 +1000

    Changed evolutionary update rules to work on networks.
    Agents now imitate an agents beliefs within their neighbourhood (including themselves) based on fitness proportional selection.
    Each agent has a chance to perturb their beliefs (once imitation has occured).

--- src/python/model.py
+++ src/python/model.py
@@ -52,8 +52,7 @@ class Model:
 
         # Create graph
         self.graph = nx.generators.random_graphs.barabasi_albert_graph(
-            number_of_agents, 5).to_directed()
-
+            number_of_agents, 5)
 
     def draw_graph(self):
         nx.draw(self.graph, with_labels=True)
@@ -111,7 +110,7 @@ class Model:
             for focal_index in self.matching_indices:
                 if game_played[focal_index]:
                     continue
-                viable_neighbours = [nbr for nbr in \
+                viable_neighbours = [nbr for nbr in
                     self.graph.adj[focal_index].keys() if not game_played[nbr]]
                 if len(viable_neighbours) > 0:
                     other_index = np.random.choice(viable_neighbours)
@@ -131,53 +130,50 @@ class Model:
         # Compute the current payoff
         self.compute_payoff(samples)
 
-        # Find the fitness probability distribution (using exponential selection intensity) for each group
-        payoff_sum_0 = np.sum(np.exp(selection_intensity*self.payoffs[0:self.number_of_0_tags]))
-        payoff_sum_1 = np.sum(np.exp(selection_intensity*self.payoffs[self.number_of_0_tags:]))
-
-        fitness_probabilities_0 = np.exp(selection_intensity*self.payoffs[0:self.number_of_0_tags])/payoff_sum_0
-        fitness_probabilities_1 = np.exp(selection_intensity*self.payoffs[self.number_of_0_tags:])/payoff_sum_1
-
         new_ingroup = []
         new_outgroup = []
-        new_payoffs = []
 
-        # Create a new generation of agents.  Sampling occurs within
-        # group only, to maintain group balance.
-        for i in range(self.number_of_agents):
-            if i < self.number_of_0_tags:
-                current_agent = np.random.choice(range(self.number_of_0_tags),
-                                                 p=fitness_probabilities_0)
-            else:
-                current_agent = np.random.choice(range(self.number_of_0_tags, self.number_of_agents),
-                                                 p=fitness_probabilities_1)
+        for current_agent in range(self.number_of_agents):
+            neighbourhood = [nbr for nbr in
+                             self.graph.adj[current_agent].keys()]
+            neighbourhood.append(current_agent)
 
-            # Perturb agents belief
+            neighbourhood_payoffs = np.array([self.payoffs[i]
+                                              for i in neighbourhood])
+
+            neighbourhood_fitness_sum = np.sum(np.exp(
+                selection_intensity*neighbourhood_payoffs))
+
+            neighbourhood_probabilities = np.exp(
+                selection_intensity*neighbourhood_payoffs) / \
+                neighbourhood_fitness_sum
+
+            agent_to_imitate = np.random.choice(neighbourhood,
+                                                p=neighbourhood_probabilities)
+
             if np.random.rand() <= perturbation_probability:
                 current_agent_new_ingroup = np.random.normal(
-                    self.ingroup[current_agent], perturbation_scale)
+                    self.ingroup[agent_to_imitate], perturbation_scale)
                 if current_agent_new_ingroup < 0:
                     current_agent_new_ingroup = 0.0
                 if current_agent_new_ingroup > 1:
                     current_agent_new_ingroup = 1.0
 
                 current_agent_new_outgroup = np.random.normal(
-                    self.outgroup[current_agent], perturbation_scale)
+                    self.outgroup[agent_to_imitate], perturbation_scale)
                 if current_agent_new_outgroup < 0:
                     current_agent_new_outgroup = 0.0
                 if current_agent_new_outgroup > 1:
                     current_agent_new_outgroup = 1.0
             else:
-                current_agent_new_ingroup = self.ingroup[current_agent]
-                current_agent_new_outgroup = self.outgroup[current_agent]
+                current_agent_new_ingroup = self.ingroup[agent_to_imitate]
+                current_agent_new_outgroup = self.outgroup[agent_to_imitate]
 
             new_ingroup.append(current_agent_new_ingroup)
             new_outgroup.append(current_agent_new_outgroup)
-            new_payoffs.append(self.payoffs[current_agent])
 
         self.ingroup = np.array(new_ingroup)
         self.outgroup = np.array(new_outgroup)
-        self.payoffs = np.array(new_payoffs)
 
     def run_simulation(self, random_seed, number_of_steps, rounds_per_step,
                        selection_intensity, perturbation_probability,
