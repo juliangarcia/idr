07fbf48de11be33436c6e4dbb57e7fcbb9464dd1
MathewBaker <bakermg@outlook.com>
2020-07-31 14:56:51 +1000

    Added the evolutionary model on a network and ability to choose which model to run.

---
+++ src/python/graph_generation.py
@@ -0,0 +1,171 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from numba import jit
+
+@jit(nopython=True)
+def permute(matching, n):
+    for i in range(n-1):
+        j = i + np.random.randint(n-i)
+        temp = matching[i]
+        matching[i] = matching[j]
+        matching[j] = temp
+
+def randomly_relabel(graph):
+    permuted_nodes = list(graph.nodes)
+    permute(permuted_nodes, len(permuted_nodes))
+    relabel_dict = dict(zip(list(graph.nodes), permuted_nodes))
+    new_edges = [(relabel_dict[source], relabel_dict[target]) for source, target in list(graph.edges())]
+
+    graph.remove_edges_from(list(graph.edges()))
+    graph.add_edges_from(new_edges)
+    nx.relabel.relabel_nodes(graph, relabel_dict)
+
+    return graph
+
+def draw_graph_circular(graph, number_of_agents, number_of_0_tags):
+        agent_0_colour = "#ff0000" # Red
+        agent_1_colour = "#0000ff" # Blue
+        nx.drawing.nx_pylab.draw_networkx(
+            graph, pos=nx.drawing.layout.circular_layout(graph),
+            nodelist=[i for i in range(number_of_agents)],
+            node_color=[agent_0_colour if i < number_of_0_tags else
+                        agent_1_colour for i in range(number_of_agents)],
+            with_labels=True)
+        plt.show()
+
+
+def gerrymandered_graph(number_of_agents, number_of_0_tags, districts, rewire_amount):
+
+    tags = np.ones(number_of_agents, dtype=int)
+
+    for i in range(number_of_0_tags):
+        tags[i] = 0
+
+    district_assignment = [0 for _ in range(number_of_agents)]
+
+    G = nx.DiGraph()
+    G.add_nodes_from([i for i in range(number_of_agents)])
+
+    for current_agent in range(number_of_agents):
+        if current_agent < number_of_0_tags:
+            current_agent_tag = 0
+        else:
+            current_agent_tag = 1
+
+        district = np.random.randint(0, len(districts[0]))
+        while districts[current_agent_tag][district] == 0:
+            district = np.random.randint(0, len(districts[0]))
+
+        district_assignment[current_agent] = district
+        districts[current_agent_tag][district] -= 1
+
+    for focal_agent in range(number_of_agents):
+        for other_agent in range(number_of_agents):
+            if district_assignment[focal_agent] == district_assignment[other_agent] and focal_agent != other_agent:
+                G.add_edge(focal_agent, other_agent)
+
+    for _ in range(rewire_amount):
+        edges = [edge for edge in G.edges]
+        edge1 = edges[np.random.choice(range(len(edges)))]
+        edge2 = edges[np.random.choice(range(len(edges)))]
+
+        while (tags[edge1[1]] != tags[edge2[1]] or # make sure target group is the same
+                edge1[1] == edge2[1] or # make sure targets are not the same
+                edge1[0] == edge2[0] or # make sure origins are not the same
+                edge1[0] == edge2[1] or # make sure target of edge2 is not origin of edge 1
+                edge2[0] == edge1[1] or # make sure target of edge1 is not origin of edge 2
+                (edge1[0], edge2[1]) in edges or # make sure swapped edges do not already exist
+                (edge2[0], edge1[1]) in edges):
+            edge1 = edges[np.random.choice(range(len(edges)))]
+            edge2 = edges[np.random.choice(range(len(edges)))]
+
+        G.remove_edge(*edge1)
+        G.remove_edge(*edge2)
+        G.add_edge(edge1[0], edge2[1])
+        G.add_edge(edge2[0], edge1[1])
+
+    return G
+
+# Some districts for 24 agents. Source: https://doi.org/10.1038/s41586-019-1507-6
+districts_1 = [[3, 3, 3, 3],
+               [3, 3, 3, 3]]
+districts_2 = [[5, 4, 2, 1],
+                [1, 2, 4,5]]
+districts_3 = [[6, 2, 2, 2],
+               [0, 4, 4, 4]]
+
+def two_communities_graph(number_of_agents, initial_number_of_0_tags, rewire_amount, add_amount):
+
+    tags = np.ones(number_of_agents, dtype=int)
+    for i in range(initial_number_of_0_tags):
+        tags[i] = 0
+
+    G = nx.complete_graph(initial_number_of_0_tags)
+    nx.to_directed(G)
+    H = nx.complete_graph(list(range(initial_number_of_0_tags, number_of_agents)))
+    nx.to_directed(H)
+
+    F = nx.disjoint_union(G,H)
+
+    for _ in range(rewire_amount):
+        edges_F = [edge for edge in F.edges]
+        edges_G = [edge for edge in G.edges]
+        edges_H = [edge for edge in H.edges]
+        edge1 = edges_G[np.random.choice(range(len(edges_G)))]
+        edge2 = edges_H[np.random.choice(range(len(edges_H)))]
+
+        while edge1 not in edges_F or \
+              edge2 not in edges_F or \
+              edge1[1] == edge2[1] or \
+                edge1[0] == edge2[0] or \
+                edge1[0] == edge2[1] or \
+                edge2[0] == edge1[1] or \
+                (edge1[0], edge2[1]) in edges_F or \
+                (edge2[0], edge1[1]) in edges_F:
+            edge1 = edges_G[np.random.choice(range(len(edges_G)))]
+            edge2 = edges_H[np.random.choice(range(len(edges_H)))]
+
+        F.remove_edge(*edge1)
+        F.remove_edge(*edge2)
+        F.add_edge(edge1[0], edge2[1])
+        F.add_edge(edge2[0], edge1[1])
+
+    for _ in range(add_amount):
+        edges_F = [edge for edge in F.edges]
+        nodes_G = [node for node in G.nodes]
+        nodes_H = [node for node in H.nodes]
+        node1 = nodes_G[np.random.choice(range(len(nodes_G)))]
+        node2 = nodes_H[np.random.choice(range(len(nodes_H)))]
+
+        while (node1, node2) in edges_F or \
+              (node2, node1) in edges_F:
+            node1 = nodes_G[np.random.choice(range(len(nodes_G)))]
+            node2 = nodes_H[np.random.choice(range(len(nodes_H)))]
+
+        direction = np.random.choice(2)
+        if direction == 0:
+            F.add_edge(node1, node2)
+        else:
+            F.add_edge(node2, node1)
+
+    return F
+
+def scale_free_graph(number_of_agents, initial_number_of_0_tags, alpha, beta, gamma):
+    
+    assert alpha + beta + gamma == 1
+
+    tags = np.ones(number_of_agents, dtype=int)
+    for i in range(initial_number_of_0_tags):
+        tags[i] = 0
+
+    G = nx.scale_free_graph(number_of_agents, alpha, beta, gamma)
+    nx.to_directed(G)
+
+    return G
+
+graph_function_map = {
+    "scale_free": lambda number_of_agents, number_of_0_tags: randomly_relabel(scale_free_graph(number_of_agents, number_of_0_tags, 0.41, 0.54, 0.05)),
+    "two_communities": lambda number_of_agents, number_of_0_tags: two_communities_graph(number_of_agents, number_of_0_tags, number_of_agents//2, number_of_agents//2)
+}
--- src/python/model.py
+++ src/python/model.py
@@ -3,16 +3,19 @@ from numba import jit
 import csv
 import json
 import sys
+import networkx as nx
+import matplotlib.pyplot as plt
 from choose_strategy_functions import *
+from graph_generation import *
 
 
-@jit(nopython=True)
-def permute(matching, n):
-    for i in range(n-1):
-        j = i + np.random.randint(n-i)
-        temp = matching[i]
-        matching[i] = matching[j]
-        matching[j] = temp
+# @jit(nopython=True)
+# def permute(matching, n):
+#     for i in range(n-1):
+#         j = i + np.random.randint(n-i)
+#         temp = matching[i]
+#         matching[i] = matching[j]
+#         matching[j] = temp
 
 class Agent:
     def __init__(self, tag, ingroup, outgroup, choose_strategy, payoff=0.0):
@@ -30,7 +33,8 @@ class Model:
     def __init__(self, number_of_agents, R, S, T, P,
                  tag0_initial_ingroup_belief, tag0_initial_outgroup_belief,
                  tag1_initial_ingroup_belief, tag1_initial_outgroup_belief,
-                 initial_number_of_0_tags, choose_strategy):
+                 initial_number_of_0_tags, choose_strategy,
+                 graph=None):
 
         # 0 is cooperate
         # 1 is defect
@@ -46,9 +50,6 @@ class Model:
 
         self.number_of_0_tags = initial_number_of_0_tags
 
-        # these contain probabilities that the in-out group
-        # will play strategy 0 - cooperate
-
         self.agents = [
             Agent(1,
                   tag1_initial_ingroup_belief, tag1_initial_outgroup_belief,
@@ -62,6 +63,20 @@ class Model:
 
         self.matching_indices = list(range(self.number_of_agents))
 
+        self.graph = graph
+
+    def draw_graph(self):
+        assert self.graph != None, "No graph available"
+        agent_0_colour = "#ff0000" # Red
+        agent_1_colour = "#0000ff" # Blue
+        nx.drawing.nx_pylab.draw_networkx(
+            self.graph, pos=nx.drawing.layout.circular_layout(self.graph),
+            nodelist=[i for i in range(self.number_of_agents)],
+            node_color=[agent_0_colour if i < self.number_of_0_tags else
+                        agent_1_colour for i in range(self.number_of_agents)],
+            with_labels=True)
+        plt.show()
+
     def encounter(self, agent_focal, agent_other):
         # assert 0 <= index_focal < self.number_of_agents
         # assert 0 <= index_other < self.number_of_agents
@@ -72,8 +87,9 @@ class Model:
 
         return self.game[choice_focal, choice_other], self.game[choice_other, choice_focal]
 
-    def compute_payoff(self, samples):
-        # self.payoffs = np.zeros(self.number_of_agents)
+    ############## Evolutionary Model on Well Mixed Population ###############
+
+    def compute_payoff_evo(self, samples):
         for agent in self.agents:
             agent.payoff = 0.0
 
@@ -88,13 +104,12 @@ class Model:
         
         for agent in self.agents:
             agent.payoff /= samples
-        # self.payoffs = self.payoffs/samples
 
-    def step(self, samples, selection_intensity, perturbation_probability=0.05,
+    def step_evo(self, samples, selection_intensity, perturbation_probability=0.05,
              perturbation_scale=0.05):
 
         # Compute the current payoff
-        self.compute_payoff(samples)
+        self.compute_payoff_evo(samples)
 
         # Find the fitness probability distribution (using exponential selection intensity) for each group
 
@@ -106,10 +121,6 @@ class Model:
 
         fitness_probabilities_0 = np.exp(selection_intensity*payoff_0)/payoff_sum_0
         fitness_probabilities_1 = np.exp(selection_intensity*payoff_1)/payoff_sum_1
-
-        # new_ingroup = []
-        # new_outgroup = []
-        # new_payoffs = []
 
         new_agents = []
 
@@ -150,7 +161,7 @@ class Model:
 
         self.agents = new_agents
 
-    def run_simulation(self, random_seed, number_of_steps, rounds_per_step,
+    def run_simulation_evo(self, random_seed, number_of_steps, rounds_per_step,
                        selection_intensity, perturbation_probability,
                        perturbation_scale, data_recording,
                        data_file_path, write_frequency):
@@ -167,7 +178,7 @@ class Model:
                 writer.writerow(header_list)
 
                 for current_step in range(number_of_steps):
-                    self.step(rounds_per_step, selection_intensity,
+                    self.step_evo(rounds_per_step, selection_intensity,
                               perturbation_probability, perturbation_scale)
 
                     if current_step % write_frequency == 0 \
@@ -184,14 +195,142 @@ class Model:
                                                             outgroup))))
         else:
             for _ in range(number_of_steps):
-                self.step(rounds_per_step, selection_intensity,
+                self.step_evo(rounds_per_step, selection_intensity,
+                          perturbation_probability, perturbation_scale)
+
+    ################# Evolutionary on a Network #########################
+    def compute_payoff_evo_network(self):
+            # self.payoffs = np.zeros(self.number_of_agents)
+            for agent in self.agents:
+                agent.payoff = 0.0
+                agent.payoff_against_0 = 0.0
+                agent.payoff_against_1 = 0.0
+
+            for focal_agent_index in range(self.number_of_agents):
+                if len(self.graph.adj[focal_agent_index].keys()) > 0:
+
+                    neighbours = [nbr for nbr in self.graph.adj[focal_agent_index].keys()]
+
+                    games_played_against_0 = 0
+                    games_played_against_1 = 0
+
+                    for neighbour_index in neighbours:
+                        payoff_focal, _ = self.encounter(self.agents[focal_agent_index], self.agents[neighbour_index])
+
+                        self.agents[focal_agent_index].payoff += payoff_focal
+
+                        if self.agents[neighbour_index].tag == 0:
+                            games_played_against_0 += 1
+                            self.agents[focal_agent_index].payoff_against_0 += payoff_focal
+                        else:
+                            games_played_against_1 += 1
+                            self.agents[focal_agent_index].payoff_against_1 += payoff_focal
+
+                    self.agents[focal_agent_index].payoff /= len(neighbours)
+                    if games_played_against_0 > 0:
+                        self.agents[focal_agent_index].payoff_against_0 /= games_played_against_0
+                    if games_played_against_1 > 0:
+                        self.agents[focal_agent_index].payoff_against_1 /= games_played_against_1
+
+    def step_evo_network(self, selection_intensity, perturbation_probability=0.05,
+             perturbation_scale=0.05):
+
+        # Compute the current payoff
+        self.compute_payoff_evo_network()
+
+        new_agents = []
+
+        for current_agent_index in range(self.number_of_agents):
+            neighbourhood = [nbr for nbr in
+                             self.graph.adj[current_agent_index].keys()]
+            neighbourhood.append(current_agent_index)
+
+            neighbourhood_payoffs = np.array([self.agents[i].payoff
+                                              for i in neighbourhood])
+
+            neighbourhood_fitness_sum = np.sum(np.exp(
+                selection_intensity*neighbourhood_payoffs))
+
+            neighbourhood_probabilities = np.exp(
+                selection_intensity*neighbourhood_payoffs) / \
+                neighbourhood_fitness_sum
+
+            agent_to_imitate_index = np.random.choice(
+                neighbourhood, p=neighbourhood_probabilities)
+
+            if np.random.rand() <= perturbation_probability:
+                current_agent_new_ingroup = np.random.normal(
+                    self.agents[agent_to_imitate_index].ingroup,
+                    perturbation_scale)
+                if current_agent_new_ingroup < 0:
+                    current_agent_new_ingroup = 0.0
+                if current_agent_new_ingroup > 1:
+                    current_agent_new_ingroup = 1.0
+
+                current_agent_new_outgroup = np.random.normal(
+                    self.agents[agent_to_imitate_index].outgroup,
+                    perturbation_scale)
+                if current_agent_new_outgroup < 0:
+                    current_agent_new_outgroup = 0.0
+                if current_agent_new_outgroup > 1:
+                    current_agent_new_outgroup = 1.0
+            else:
+                current_agent_new_ingroup = self.agents[agent_to_imitate_index]\
+                    .ingroup
+                current_agent_new_outgroup = self.agents[agent_to_imitate_index]\
+                    .outgroup
+
+            new_agents.append(
+                Agent(self.agents[current_agent_index].tag,
+                      current_agent_new_ingroup, current_agent_new_outgroup,
+                      self.agents[current_agent_index].choose_strategy_func,
+                      self.agents[current_agent_index].payoff))
+
+        self.agents = new_agents
+
+    def run_simulation_evo_network(self, random_seed, number_of_steps,
+                       selection_intensity, perturbation_probability,
+                       perturbation_scale, data_recording,
+                       data_file_path, write_frequency):
+
+        np.random.seed(random_seed)
+
+        if data_recording:
+            with open(data_file_path, 'w', newline='\n') as output_file:
+                writer = csv.writer(output_file)
+                header_list = ["T"] + ["P" + str(i) for i in range(self.number_of_agents)] + \
+                              ["I" + str(i) for i in range(self.number_of_agents)] + \
+                              ["O" + str(i) for i in range(self.number_of_agents)]
+                writer.writerow(header_list)
+
+                for current_step in range(number_of_steps):
+                    self.step_evo_network(selection_intensity,
+                              perturbation_probability, perturbation_scale)
+
+                    if current_step % write_frequency == 0 \
+                            or current_step == number_of_steps - 1:
+                        payoffs = np.array([agent.payoff
+                                            for agent in self.agents])
+                        ingroup = np.array([agent.ingroup
+                                            for agent in self.agents])
+                        outgroup = np.array([agent.outgroup
+                                             for agent in self.agents])
+                        writer.writerow(np.append([current_step],
+                                            np.append(payoffs,
+                                                  np.append(ingroup,
+                                                            outgroup))))
+        else:
+            for _ in range(number_of_steps):
+                self.step_evo_network(selection_intensity,
                           perturbation_probability, perturbation_scale)
 
 
 def main(config_file_path):
     with open(config_file_path, 'r') as config_file:
         config = json.load(config_file)
-    model = Model(config["number_of_agents"],
+
+    # Setup model call with standardised parameters
+    model_call = lambda *args: Model(config["number_of_agents"],
                   config["R"], config["S"],
                   config["T"], config["P"],
                   config["tag0_initial_ingroup_belief"],
@@ -199,9 +338,25 @@ def main(config_file_path):
                   config["tag1_initial_ingroup_belief"],
                   config["tag1_initial_outgroup_belief"],
                   config["initial_number_of_0_tags"],
-                  choose_strategy_map[config["choose_strategy"]])
-    model.run_simulation(config["random_seed"], config["number_of_steps"],
-                         config["rounds_per_step"],
+                  choose_strategy_map[config["choose_strategy"]],
+                  *args)
+
+    if config["model_type"] == 'evo':
+        model = model_call()
+        model.run_simulation_evo(config["random_seed"], config["number_of_steps"],
+                            config["rounds_per_step"],
+                            config["selection_intensity"],
+                            config["perturbation_probability"],
+                            config["perturbation_scale"], config["data_recording"],
+                            config["data_file_path"], config["write_frequency"])
+
+    if config["model_type"] == 'evo_network':
+        graph_func = graph_function_map[config["graph_type"]]
+        graph = graph_func(config["number_of_agents"], config["initial_number_of_0_tags"])
+        with open(config["graph_file_path"], 'w') as out_file:
+            json.dump(nx.readwrite.json_graph.node_link_data(graph), out_file, indent=4)
+        model = model_call(graph)
+        model.run_simulation_evo_network(config["random_seed"], config["number_of_steps"],
                          config["selection_intensity"],
                          config["perturbation_probability"],
                          config["perturbation_scale"], config["data_recording"],
