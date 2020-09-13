6198002a389cceb094a6ac1cca718b3310a0c52f
Frances Cameron-Muller <58795509+francescameronmuller@users.noreply.github.com>
2020-09-13 10:29:17 +1000

    Create test_csv_writer.py

---
+++ data/test_cluster_run/test_csv_writer.py
@@ -0,0 +1,17 @@
+import numpy as np
+from numba import jit
+import csv
+import json
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from choose_strategy_functions import *
+from graph_generation import *
+
+data_file_path = "test.csv"
+with open(data_file_path, 'w', newline='\n') as output_file:
+                writer = csv.writer(output_file)
+                writer.writerow("Testing")
